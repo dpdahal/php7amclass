5a6c59bcf6389070527003ec0a6cb86ac78581fc
dpdahal <dp48dahal@gmail.com>
2018-04-22 08:17:52 +0545

    github add

---
+++ introduction.php
@@ -0,0 +1,65 @@
+<?php
+
+//phpinfo();
+
+//echo PHP_VERSION;
+
+//echo "php class ";
+
+
+//$name='admin';
+
+//echo "your name is $name";
+
+//9801169146
+
+$x=10;
+$y='10';
+
+//echo gettype($y);
+//
+//die;
+
+if($x===$y){
+    echo "yes";
+}else{
+    echo "no";
+}
+
+//
+//$userName='admin';
+//$password='admin002';
+//
+//if($userName==='admin' && $password==='admin002'){
+//    echo "welcome";
+//
+//}else{
+//    echo "username & password not match";
+//}
+
+
+$x=10;
+$y=20;
+$z=30;
+
+if($x>$y && $x>$z){
+    echo " X is large";
+}elseif ($y>$x && $y>$z){
+
+    echo "Y is large";
+}else{
+    echo " Z is large";
+}
+
+
+
+
+
+
+
+
+?>
+
+
+
+
